b4ac57b8250e316115358de20ad474757af36cc5
Nick Harding <njh@well.ox.ac.uk>
2015-05-12 12:05:11 +0100

    first commit

---
+++ notebook.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+anaconda3/bin/ipython notebook --no-browser --ip=0.0.0.0 --port=8888
---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -v ${HOME}/git/ag1000g:/ag1000g -p 31777:8888 -v /data:/data --name ipython_demo base
